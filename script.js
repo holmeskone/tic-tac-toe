373267f554294b8124fd12a824aa816719e2ea3e
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-10-14 17:02:30 +0100

    Add draw checker, add request to restart or not-restart, removed unnecessary comments

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-//IIFE to create newBoard
-
-// const gameBoard = (function() { 
-//     const rows = 3;
-//     const columns = 3;
-//     const initialBoard = [];
-
-//     return {
-//         newBoard: function (){
-//             //Looping through to create our board
-//             for (i=0; i<rows; i++){
-//                 initialBoard[i] = [];
-//                 for (j=0; j<columns; j++){
-//                     initialBoard[i].push([j]);
-//                 };
-//             };
-//             cleanSlate = initialBoard;
-//             console.log(cleanSlate);      
-//         }
-//     };
-// })();
-
-
 const gameBoard = (function(){
     const rows = 3;
     const columns = 3;
@@ -37,14 +14,18 @@ const gameBoard = (function(){
     }
 
     let playingBoard = startBoard();
-    function currentBoard(user, row,cell,symbol){
+    
+    function currentBoard(user,row,cell,symbol){
         if(playingBoard[row][cell]!=''){
             console.log('Cell already taken');
         }
+        else if (winnerChecker === 'Winner'){
+            playingBoard = startBoard()
+        }
         else{
             playingBoard[row][cell]=(symbol);
+            console.log(playingBoard);
         }
-        console.log(playingBoard);
         winnerChecker(user, symbol);
     }
 
@@ -52,18 +33,60 @@ const gameBoard = (function(){
         let j = 0;
         //Diagonal Winner
         if (playingBoard[j][j] === symbol && playingBoard[j+1][j+1] === symbol && playingBoard[j+2][j+2] === symbol){
-            console.log(`Player ${user} wins with ${symbol}`)
+            console.log(`Player ${user} wins with ${symbol}`);
+            let restart = prompt('Would you like to play again? Y/N');
+                if(restart === 'Y'){
+                    playingBoard = startBoard();
+                    player1.movement();
+                }
+                else{
+                    console.log(playingBoard);
+                }
         }
+        // A draw!
+        else if(playingBoard[j][i] !='' && playingBoard[j+1][i+1] !='' && playingBoard[j+2][i+2] !='' &&
+            playingBoard[j+1][i] !='' && playingBoard[j+1][i+1] !='' && playingBoard[j+1][i+2] !='' &&
+            playingBoard[j+2][i] !='' && playingBoard[j+2][i+1] !='' && playingBoard[j+2][i+2] !=''){
+            console.log(`It's a draw!`)
+            let restart = prompt('Would you like to play again? Y/N');
+                if(restart === 'Y'){
+                    playingBoard = startBoard();
+                    player1.movement();
+                }
+                else{
+                    console.log(playingBoard);
+                }
+            }
+
         for(i = 0; i<3; i++){
                 //Row Winner
                 if(playingBoard[i][j] === symbol && playingBoard[i][j+1] === symbol && playingBoard[i][j+2] === symbol){
-                    console.log(`Player ${user} wins with ${symbol}`)}
+                    console.log(`Player ${user} wins with ${symbol}`);
+                    let restart = prompt('Would you like to play again? Y/N');
+                    if(restart === 'Y'){
+                    playingBoard = startBoard();
+                    player1.movement();
+                    }
+                    else{
+                    console.log(playingBoard);
+                    }
+                }
+                    
                 //Column Winner
                 else if (playingBoard[j][i] === symbol && playingBoard[j+1][i] === symbol && playingBoard[j+2][i] === symbol){
-                console.log(`Player ${user} wins with ${symbol}`)
+                console.log(`Player ${user} wins with ${symbol}`);
+                let restart = prompt('Would you like to play again? Y/N');
+                    if(restart === 'Y'){
+                    playingBoard = startBoard();
+                    player1.movement();
+                    }
+                    else{
+                    console.log(playingBoard);
+                    }
                 }
+            }
+
         }
-    }
 
     return{
         startBoard: startBoard,
@@ -73,7 +96,7 @@ const gameBoard = (function(){
 
 
 
-//Create users
+//Create users 
 function player(user,symbol){
     const movements = [];
     return {
@@ -86,39 +109,10 @@ function player(user,symbol){
             console.log(`Player ${user} chose row ${row} and cell ${cell}`);
             histMovements = movements.push(user);
             console.log(histMovements);
-            // for (i=0; i<4; i++){
-            //     if(this.user === 1){
-            //         player2.turn();
-            //         console.log(i);
-            //         }
-            //     else{
-            //         player1.turn();
-            //         console.log(i);
-            //         }
-            // }
         },
     }
 }
+
 const player1 = player(1,'X');
 const player2 = player(2,'O');
 
-
-// function movement(user, cell){
-//     let row = [];
-//     return {
-//         user: user,
-//         cell: cell,
-//         selection: function (){
-//             row.push(cell)
-//             console.log(row.push(cell));
-//         }
-//     }
-// }
-
-// const user1 = player(1,'X');
-// user1.create();
-// const user2 = player(2,'O');
-// user2.create();
-
-// user3 = movement(1,1);
-// user3.selection();
