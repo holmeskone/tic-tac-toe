d15a0117329f9e6f3d465933bd9d25c3766e8bd0
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-10-25 15:42:34 +0200

    Add change of cell background color when there's a winner, and refresh page when refresh button is clicked

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ const gameBoard = (function(){
     return initialBoard;
     }
 
+    //Create users 
+    function player(user,symbol){
+    return {
+        user: user,
+        symbol: symbol,}}
+
     let playingBoard = startBoard();
     const player1 = player(1,'X');
     const player2 = player(2,'O');
@@ -22,7 +28,7 @@ const gameBoard = (function(){
             console.log('Cell already taken');
         }
         else if (winnerChecker === 'Winner'){
-            playingBoard = startBoard()
+            playingBoard = startBoard();
             winnerChecker(user, symbol);
 
         }
@@ -36,19 +42,27 @@ const gameBoard = (function(){
     function winnerChecker(user, symbol){
         let j = 0;
         let i = 0;
-        const winnerText = document.getElementById('winner-text');
         //Diagonal Winner
         if (playingBoard[j][j] === symbol && playingBoard[j+1][j+1] === symbol && playingBoard[j+2][j+2] === symbol){
-            console.log(`Player ${user} wins with ${symbol}`);
-            // winnerText.style.display = "block";
-            let restart = prompt(`WE HAVE A WINNER, PLAYER ${user} WINS! Would you like to play again? Y/N`);
-                if(restart === 'Y'){
-                    playingBoard = startBoard();
-                    player1.movement();
-                }
-                else{
-                    console.log(playingBoard);
-                }
+            document.getElementById('00').style.background = 'blue';
+            document.getElementById('00').style.color = 'white';
+            document.getElementById('11').style.background = 'blue';
+            document.getElementById('11').style.color = 'white';
+            document.getElementById('22').style.background = 'blue';
+            document.getElementById('22').style.color = 'white';
+            // Next game
+            const startButton=document.getElementById("start");
+            startButton.addEventListener('click', function(e){
+                console.log('Hello Star')
+            })
+
+            //Restart
+            const restartButton=document.getElementById("restart");
+            restartButton.addEventListener('click', function(e){
+                console.log('helloworldRestart')
+                location.reload();
+            })
+
         }
         // A draw!
         else if(playingBoard[j][i] !='' && playingBoard[j+1][i+1] !='' && playingBoard[j+2][i+2] !='' &&
@@ -58,7 +72,6 @@ const gameBoard = (function(){
             let restart = prompt('Would you like to play again? Y/N');
                 if(restart === 'Y'){
                     playingBoard = startBoard();
-                    player1.movement();
                 }
                 else{
                     console.log(playingBoard);
@@ -68,199 +81,116 @@ const gameBoard = (function(){
         for(i = 0; i<3; i++){
                 //Row Winner
                 if(playingBoard[i][j] === symbol && playingBoard[i][j+1] === symbol && playingBoard[i][j+2] === symbol){
+                    document.getElementById(`${i}${j}`).style.background = 'blue';
+                    document.getElementById(`${i}${j}`).style.color = 'white';
+                    document.getElementById(`${i}${j+1}`).style.background = 'blue';
+                    document.getElementById(`${i}${j+1}`).style.color = 'white';
+                    document.getElementById(`${i}${j+2}`).style.background = 'blue';
+                    document.getElementById(`${i}${j+2}`).style.color = 'white';
                     console.log(`Player ${user} wins with ${symbol}`);
-                    let restart = prompt('Would you like to play again? Y/N');
-                    if(restart === 'Y'){
-                    playingBoard = startBoard();
-                    player1.movement();
-                    }
-                    else{
-                    console.log(playingBoard);
-                    }
+                     // Next game
+                    const startButton=document.getElementById("start");
+                    startButton.addEventListener('click', function(e){
+                        console.log('Hello Star')
+                    })
+
+                    //Restart
+                    const restartButton=document.getElementById("restart");
+                    restartButton.addEventListener('click', function(e){
+                        console.log('helloworldRestart')
+                        location.reload();
+                    })
                 }
                     
                 //Column Winner
                 else if (playingBoard[j][i] === symbol && playingBoard[j+1][i] === symbol && playingBoard[j+2][i] === symbol){
-                console.log(`Player ${user} wins with ${symbol}`);
-                let restart = prompt('Would you like to play again? Y/N');
-                    if(restart === 'Y'){
-                    playingBoard = startBoard();
-                    player1.movement();
-                    }
-                    else{
-                    console.log(playingBoard);
-                    }
+                    document.getElementById(`${j}${i}`).style.background = 'blue';
+                    document.getElementById(`${j}${i}`).style.color = 'white';
+                    document.getElementById(`${j+1}${i}`).style.background = 'blue';
+                    document.getElementById(`${j+1}${i}`).style.color = 'white';
+                    document.getElementById(`${j+2}${i}`).style.background = 'blue';
+                    document.getElementById(`${j+2}${i}`).style.color = 'white';
+                    console.log(`Player ${user} wins with ${symbol}`);
+                    // Next game
+                    const startButton=document.getElementById("start");
+                    startButton.addEventListener('click', function(e){
+                        console.log('Hello Star')
+                    })
+
+                    //Restart
+                    const restartButton=document.getElementById("restart");
+                    restartButton.addEventListener('click', function(e){
+                        console.log('helloworldRestart')
+                        location.reload();
+                    })
                 }
             }
 
         }
 
+
+    const movement = [];
+    let playerNumberOne = 1;
+    let playerNumberTwo = 2;
+        
+        
+    function turn(event) {
+        // document.addEventListener("click", function(event) {
+        if (event.target.classList.contains("cell")) {
+            if(movement.length === 0){
+                movement.push(1);
+                console.log(movement);
+                let playerSymbol = "X";
+                event.target.innerHTML = playerSymbol;
+                let selectedRow = event.target.dataset.row;
+                let selectedColumn = event.target.dataset.column;
+                console.log(`Player number ${playerNumberOne} has selected the row ${selectedRow} and column ${selectedColumn}`);
+                gameBoard.currentBoard(playerNumberOne, selectedRow,selectedColumn,playerSymbol);
+                }
+            else if (movement[(movement.length)-1] != 1){
+                movement.push(1);
+                console.log(movement);
+                let playerSymbol = "X";
+                event.target.innerHTML = playerSymbol;
+                let selectedRow = event.target.dataset.row;
+                let selectedColumn = event.target.dataset.column;
+                console.log(`Player number ${playerNumberOne} has selected the row ${selectedRow} and column ${selectedColumn}`);
+                gameBoard.currentBoard(playerNumberOne, selectedRow,selectedColumn,playerSymbol);
+                }
+    
+            else if (movement[(movement.length)-1] === 1){
+                movement.push(2);
+                console.log(movement);
+                let playerSymbol = "O";
+                event.target.innerHTML = playerSymbol;
+                let selectedRow = event.target.dataset.row;
+                let selectedColumn = event.target.dataset.column;
+                console.log(`Player number ${playerNumberTwo} has selected the row ${selectedRow} and column ${selectedColumn}`);
+                gameBoard.currentBoard(playerNumberTwo, selectedRow,selectedColumn,playerSymbol);
+                }
+            }
+        }
+    document.addEventListener("click", turn);
     return{
         startBoard: startBoard,
-        currentBoard: currentBoard
+        player:player,
+        currentBoard: currentBoard, 
+        winnerChecker: winnerChecker,
+        turn: turn,
     };
 }) ();
 
-const movement = [];
-let playerNumberOne = 1;
-let playerNumberTwo = 2;
 
-
-function turn(event) {
-    // document.addEventListener("click", function(event) {
-    if (event.target.classList.contains("cell")) {
-        if(movement.length === 0){
-            movement.push(1);
-            console.log(movement);
-            let playerSymbol = "X";
-            event.target.innerHTML = playerSymbol;
-            let selectedRow = event.target.dataset.row;
-            let selectedColumn = event.target.dataset.column;
-            console.log(`Player number ${playerNumberOne} has selected the row ${selectedRow} and column ${selectedColumn}`);
-            gameBoard.currentBoard(playerNumberOne, selectedRow,selectedColumn,playerSymbol);
-            }
-        else if (movement[(movement.length)-1] != 1){
-            movement.push(1);
-            console.log(movement);
-            let playerSymbol = "X";
-            event.target.innerHTML = playerSymbol;
-            let selectedRow = event.target.dataset.row;
-            let selectedColumn = event.target.dataset.column;
-            console.log(`Player number ${playerNumberOne} has selected the row ${selectedRow} and column ${selectedColumn}`);
-            gameBoard.currentBoard(playerNumberOne, selectedRow,selectedColumn,playerSymbol);
-            }
-
-        else if (movement[(movement.length)-1] === 1){
-            movement.push(2);
-            console.log(movement);
-            let playerSymbol = "O";
-            event.target.innerHTML = playerSymbol;
-            let selectedRow = event.target.dataset.row;
-            let selectedColumn = event.target.dataset.column;
-            console.log(`Player number ${playerNumberTwo} has selected the row ${selectedRow} and column ${selectedColumn}`);
-            gameBoard.currentBoard(playerNumberTwo, selectedRow,selectedColumn,playerSymbol);
-            }
-        }
-    }
-document.addEventListener("click", turn);
-
-//Create users 
-function player(user,symbol){
-    return {
-        user: user,
-        symbol: symbol,
-        movement: function(){
-            let row = selectedRow;
-            console.log(row);          
-            gameBoard.currentBoard(user, row,cell,symbol);
-            console.log(`Player ${user} chose row ${row} and cell ${cell}`);
-        },
-    }
-}
-
-
-
-
-
-
-//     //Alternating turns
-//     if (movement.length === 0) {
-//         movement.push(playerNumberOne);
-//         console.log(playerNumberOne);
-//         document.addEventListener("click", function(event) {
-//             // Only change the innerHTML if the clicked element has the class "cell"
-//             if (event.target.classList.contains("cell") ) {
-//                 let playerSymbol = "X";
-//                 event.target.innerHTML = playerSymbol;
-//                 let selectedRow = event.target.dataset.row;
-//                 let selectedColumn = event.target.dataset.column;
-//                 console.log(`Player number ${playerNumberOne} has selected the row ${selectedRow} and column ${selectedColumn}`);
-//                 gameBoard.currentBoard(playerNumberOne, selectedRow,selectedColumn,playerSymbol);
-//             }
-//         });
-//     }
-//     else if (movement[(movement.length) - 1] === 1) {
-//         let playerNumberTwo = 2;
-//         console.log(playerNumberTwo);
-//         document.addEventListener("click", function(event) {
-//             // Only change the innerHTML if the clicked element has the class "cell"
-//             if (event.target.classList.contains("cell")) {
-//                 let playerSymbol = "O";
-//                 event.target.innerHTML = playerSymbol;
-//                 let selectedRow = event.target.dataset.row;
-//                 let selectedColumn = event.target.dataset.column;
-//                 console.log(`Player number ${playerNumberTwo} has selected the row ${selectedRow} and column ${selectedColumn}`);
-//                 gameBoard.currentBoard(playerNumberTwo, selectedRow,selectedColumn,playerSymbol);
-//             }
-//         });
-//     }
-//     else {
-//         movement.push(playerNumberOne);
-//         console.log(playerNumberOne);
-//         document.addEventListener("click", function(event) {
-//             // Only change the innerHTML if the clicked element has the class "cell"
-//             if (event.target.classList.contains("cell")) {
-//                 let playerSymbol = "X";
-//                 event.target.innerHTML = playerSymbol;
-//                 console.log(event.target.dataset.row);
-//                 let selectedRow = event.target.dataset.row;
-//                 let selectedColumn = event.target.dataset.column;
-//                 console.log(`The else statement: Player number ${playerNumberOne} has selected the row ${selectedRow} and column ${selectedColumn}`);
-//                 gameBoard.currentBoard(playerNumberOne, selectedRow,selectedColumn,playerSymbol);
-//             }
-//         });
+// //Create users 
+// function player(user,symbol){
+//     return {
+//         user: user,
+//         symbol: symbol,
+//         movement: function(){
+//             let row = selectedRow;
+//             gameBoard.currentBoard(user, row,cell,symbol);
+//             console.log(`Player ${user} chose row ${row} and cell ${cell}`);
+//         },
 //     }
 // }
 
-
-
-// function turn() {
-//     console.log(movement);
-//     //Alternating turns
-//     if (movement.length === 0) {
-//         movement.push(playerNumberOne);
-//         console.log(playerNumberOne);
-//         document.addEventListener("click", function(event) {
-//             // Only change the innerHTML if the clicked element has the class "cell"
-//             if (event.target.classList.contains("cell") ) {
-//                 let playerSymbol = "X";
-//                 event.target.innerHTML = playerSymbol;
-//                 let selectedRow = event.target.dataset.row;
-//                 let selectedColumn = event.target.dataset.column;
-//                 console.log(`Player number ${playerNumberOne} has selected the row ${selectedRow} and column ${selectedColumn}`);
-//                 gameBoard.currentBoard(playerNumberOne, selectedRow,selectedColumn,playerSymbol);
-//             }
-//         });
-//     }
-//     else if (movement[(movement.length) - 1] === 1) {
-//         let playerNumberTwo = 2;
-//         console.log(playerNumberTwo);
-//         document.addEventListener("click", function(event) {
-//             // Only change the innerHTML if the clicked element has the class "cell"
-//             if (event.target.classList.contains("cell")) {
-//                 let playerSymbol = "O";
-//                 event.target.innerHTML = playerSymbol;
-//                 let selectedRow = event.target.dataset.row;
-//                 let selectedColumn = event.target.dataset.column;
-//                 console.log(`Player number ${playerNumberTwo} has selected the row ${selectedRow} and column ${selectedColumn}`);
-//                 gameBoard.currentBoard(playerNumberTwo, selectedRow,selectedColumn,playerSymbol);
-//             }
-//         });
-//     }
-//     else {
-//         movement.push(playerNumberOne);
-//         console.log(playerNumberOne);
-//         document.addEventListener("click", function(event) {
-//             // Only change the innerHTML if the clicked element has the class "cell"
-//             if (event.target.classList.contains("cell")) {
-//                 let playerSymbol = "X";
-//                 event.target.innerHTML = playerSymbol;
-//                 console.log(event.target.dataset.row);
-//                 let selectedRow = event.target.dataset.row;
-//                 let selectedColumn = event.target.dataset.column;
-//                 console.log(`The else statement: Player number ${playerNumberOne} has selected the row ${selectedRow} and column ${selectedColumn}`);
-//                 gameBoard.currentBoard(playerNumberOne, selectedRow,selectedColumn,playerSymbol);
-//             }
-//         });
-//     }
-// }
